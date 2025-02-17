ac74d85137e70cee2085a02a683501c73c364ee0
LivingTree <aryarega2811@gmail.com>
2025-02-17 12:44:12 +0700

    day3nilai.php

--- day3nilai.php
+++ day3nilai.php
@@ -4,15 +4,15 @@ $nilai = -3;
 if($nilai <= 100 && $nilai >= 90) {
     echo "A";}
 
-if($nilai <= 89 && $nilai >= 80) {
+else if($nilai <= 89 && $nilai >= 80) {
     echo "B";}
 
-if($nilai <= 79 && $nilai >= 70) {
+else if($nilai <= 79 && $nilai >= 70) {
     echo "C";}
 
 else if ($nilai <= 69 && $nilai > 0) {
     echo "D";}
 
 else {
-    echo "nilai wajib di atas 0";}
+    echo "nilai wajib di antara 0";}
 ?>
